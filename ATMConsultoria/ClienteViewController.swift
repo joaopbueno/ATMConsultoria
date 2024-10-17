67b5e9a32529ffe28dcbbc14241f85843aa294b1
joaopbueno <mrjoao16@gmail.com>
2024-10-17 17:12:25 -0300

    Pagina Main do projeto

---
+++ ATMConsultoria/ClienteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ClienteViewController.swift
+//  ATMConsultoria
+//
+//  Created by João Bueno on 17/10/24.
+//
+
+import SwiftUI
+
+struct ClienteViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClienteViewController()
+}
